1e79d360759151c5df8a7dbbf57615e4315e7b59
Rafael <rafaasimi@hotmail.com>
2021-09-04 22:22:39 -0300

    Added og-image

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -15,14 +15,14 @@ export default class MyDocument extends Document {
           <meta property="og:url" content="https://rafaelsimionato.dev/" />
           <meta property="og:title" content="Rafael Simionato - Front-end Developer" />
           <meta property="og:description" content="I’m passionate about front-end development and how everything can be I transformed through lines of code on amazing screens." />
-          <meta property="og:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png" />
+          <meta property="og:image" content="/og-image.jpg" />
 
           {/* <!-- Twitter --> */}
           <meta property="twitter:card" content="summary_large_image" />
           <meta property="twitter:url" content="https://rafaelsimionato.dev/" />
           <meta property="twitter:title" content="Rafael Simionato - Front-end Developer" />
           <meta property="twitter:description" content="I’m passionate about front-end development and how everything can be I transformed through lines of code on amazing screens." />
-          <meta property="twitter:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png" />
+          <meta property="twitter:image" content="/og-image.jpg" />
           
           <link rel="shortcut icon" href="/favicon.png" type="image/png" />
 
