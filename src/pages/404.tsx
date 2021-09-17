40e41540f6c4058b5ebd355c43af2e5ff66ad963
Rafael <rafaasimi@hotmail.com>
2021-09-16 22:01:16 -0300

    Created and stylized 404 error page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,3 +1,36 @@
+import Image from "next/image";
+import Link from "next/link";
+
+import styles from "../styles/404.module.scss";
+
+import astronautImg from "../assets/astronaut.svg";
+
 export default function Custom404() {
-  return <h1>404 - Page Not Found</h1>;
+  type ComponentProps = React.PropsWithChildren<{
+    index: any;
+    value: any;
+  }> &
+    React.DetailedHTMLProps<
+      React.HTMLAttributes<HTMLDivElement>,
+      HTMLDivElement
+    >;
+
+  return (
+    <section id={styles.page404}>
+      <div className={styles.content}>
+        <div className={styles.description}>
+          <h1>404</h1>
+          <p>
+            Hey, stop! Looks like what you were looking for is no longer here.
+          </p>
+          <Link href="/" passHref>
+            <a className="button primary-button">Return</a>
+          </Link>
+        </div>
+        <div className={styles.image}>
+          <Image src={astronautImg} alt="Astronaut" />
+        </div>
+      </div>
+    </section>
+  );
 }
